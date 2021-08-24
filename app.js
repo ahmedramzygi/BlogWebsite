f31e3c76f5817492c1ada60334a95cae5b95b939
ahmedramzygi <17P8229@eng.asu.edu.eg>
2021-08-24 14:53:39 +0200

    Adding mongoDB to our site

--- app.js
+++ app.js
@@ -1,30 +1,52 @@
 const express = require('express');
+const Blog=require('./models/Blog');
 
 // express app
 const app = express();
+const mongoose=require('mongoose');
 
 // listen for requests
 app.listen(3000);
+const dbURI='mongodb+srv://Ahmed:test@blog.gwbak.mongodb.net/Blog?retryWrites=true&w=majority'
+//
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(4000))// To make sure the db is connected before we start our project.
+  .catch(err => console.log(err));
+
 
 // register view engine
 app.set('view engine', 'ejs');
-// app.set('views', 'myviews');
-
-app.get('/', (req, res) => {
- const blogs=[
-  {title: 'Workout Routine', snippet: 'I start with exercising  early in the morning at the gym'},
- {title: 'Food plan', snippet: 'I believe that the most important factor to have a good shape is to eat healthy balanced meals througout the day'},
- {title: 'Courses', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-];
-  res.render('index', { title: 'Home',blogs:blogs  });
-});
-
+app.get('/',(req,res)=>{
+  res.redirect('/blogs');
+})
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
 });
+app.get('/blogs',(req,res)=>{
+  Blog.find().sort({createdAt:-1})
+  .then((result)=>{
+    console.log(result);
+    res.render('index',{title: 'All Blogs',blogs:result})
+  })
+  .catch((err)=>{
+    console.log(err);
+  })
+})
 
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new blog' });
+});
+
+
+app.get('/all-blogs', (req, res) => {
+  Blog.find()
+    .then(result => {
+      res.send(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 const path = require('path')
 app.use(express.static('public'));
@@ -33,3 +55,4 @@ app.use(express.static('public'));
 app.use((req, res) => {
   res.status(404).render('404', { title: '404' });
 });
+ 
